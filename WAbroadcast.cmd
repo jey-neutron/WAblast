86a567f19b0beff59157824d720cbbd660f567c3
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-06-13 15:17:27 +0800

    Update WAbroadcast.cmd

--- WAbroadcast.cmd
+++ WAbroadcast.cmd
@@ -1,4 +1,6 @@
 ::place this file in same dir as WABOT
 ::req= node, nodemon
-start http://localhost:8000 & nodemon index_broadcast.js
+echo # Installing libraries
+::remove until ..npm install for using notupdated dependencies
+npm i -g npm-check-updates & ncu -u & npm install & start http://localhost:8000 & nodemon index_broadcast.js
 cmd /k
