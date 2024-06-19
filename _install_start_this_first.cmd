d513686486e62c4a2242a4338d119f3b4e147818
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-06-20 06:56:34 +0800

    Delete a file

--- _install_start_this_first.cmd
+++
@@ -1,8 +0,0 @@
-::place this file in same dir as WABOT
-::req= node, nodemon
-echo # Installing libraries
-::remove until ..npm install for using notupdated dependencies
-::npm audit fix --force
-npm install github:pedroslopez/whatsapp-web.js#webpack-exodus
-
-cmd /k
