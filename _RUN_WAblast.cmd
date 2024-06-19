ace154c5035e9c615aea6bcc28095907aaf9ee85
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-06-20 06:57:38 +0800

    Update _RUN_WAblast.cmd

--- _RUN_WAblast.cmd
+++ _RUN_WAblast.cmd
@@ -1,7 +1,9 @@
 ::place this file in same dir as WABOT
 ::req= node, nodemon
 echo # Installing libraries
-::remove until ..npm install &.. for using not updated dependencies
+::use below for using updated dependencies
 ::npm i whatsapp-web.js & npm i -g npm-check-updates & ncu -u & 
+::below for stable waweb
+::npm install github:pedroslopez/whatsapp-web.js#webpack-exodus
 npm install -g nodemon & npm install & start http://localhost:8000 & nodemon index_broadcast.js
 cmd /k
