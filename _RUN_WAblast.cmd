8be2787c2d598cc7f45dee75baee420661888fb0
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-10-16 14:41:20 +0800

    Update _RUN_WAblast.cmd

--- _RUN_WAblast.cmd
+++ _RUN_WAblast.cmd
@@ -1,9 +1,11 @@
 ::place this file in same dir as WABOT
 ::req= node, nodemon
 echo # Installing libraries
-::use below for using updated dependencies
+::- use below for using updated dependencies
 ::npm i whatsapp-web.js & npm i -g npm-check-updates & ncu -u & 
-::below for stable waweb
-::npm install github:pedroslopez/whatsapp-web.js#webpack-exodus
+::- below for stable waweb [USE THESS]
+::npm i whatsapp-web.js@1.24.0 & npm i -g npm-check-updates & ncu -u & 
+::- node fetch version used
+::npm install node-fetch@2.6.6
 npm install -g nodemon & npm install & start http://localhost:8000 & nodemon index_broadcast.js
 cmd /k
